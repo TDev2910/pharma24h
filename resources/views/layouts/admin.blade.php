ce1ba390b7355f6356c9a43dd4fda0071f33935f
DevPham <phamchitrong2910@gmail.com>
2025-07-26 17:33:36 +0700

    Cập nhật layout admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,18 +4,21 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'Admin Dashboard')</title>
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+    <!-- Menu trên cùng --> 
     <nav class="navbar">
         <div class="nav-menu">
-            <a href="#" class="nav-item active">Tổng quan</a>
+            <a href="{{ route('admin.dashboard') }}" class="nav-item active">Tổng Quan</a>
             <div class="nav-item dropdown">
-                Hàng hóa
+                <span class="nav-item">Hàng hóa</span>
                 <div class="nav-dropdown">
                     <div class="dropdown-col">
                         <div class="dropdown-title">Hàng hóa</div>
-                        <a href="#" class="dropdown-link">Danh sách hàng hóa</a>
+                        <a href="{{ route('admin.products.index') }}" class="dropdown-link">Danh sách hàng hóa</a>
                         <a href="#" class="dropdown-link">Danh sách thuốc</a>
                         <a href="#" class="dropdown-link">Thiết lập giá</a>
                     </div>
@@ -28,20 +31,21 @@
                         <div class="dropdown-title">Nhập hàng</div>
                         <a href="#" class="dropdown-link">Nhà cung cấp</a>
                         <a href="#" class="dropdown-link">Nhập hàng</a>
+                        <a href="#" class="dropdown-link">Trả hàng nhập</a>
                     </div>
                 </div>
             </div>
             <a href="#" class="nav-item">Đơn hàng</a>
-            <a href="#" class="nav-item">Sản phẩm</a>
             <a href="#" class="nav-item">Khách hàng</a>
+            <a href="#" class="nav-item">Bác sĩ</a>
             <a href="#" class="nav-item">Nhân viên</a>
-            <a href="#" class="nav-item">Báo cáo</a>
+            <a href="#" class="nav-item">Sổ quỹ</a>
         </div>
         <button class="sell-btn">
             <i class="fas fa-cart-shopping cart-icon"></i> Bán hàng
         </button>
     </nav>
-    <div class="container">
+    <div class="container-fluid">
         @yield('content')
     </div>
 </body>
